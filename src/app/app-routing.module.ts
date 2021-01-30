5c9b326ac21d80d65d4d3cb922a658c5217ed9ff
Juan Fermin Dev <juanfob.dev@gmail.com>
2021-01-30 15:03:35 +0100

    until 2.1 point finished - page 17

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+const routes: Routes = [  
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'introduction',
     pathMatch: 'full'
+  },
+  {
+    path: 'introduction',
+    loadChildren: () => import('./pages/introduction/introduction.module').then( m => m.IntroductionPageModule)
   },
 ];
 
--- src/app/pages/introduction/introduction.page.spec.ts
+++ src/app/pages/introduction/introduction.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { IntroductionPage } from './introduction.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('IntroductionPage', () => {
+  let component: IntroductionPage;
+  let fixture: ComponentFixture<IntroductionPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ IntroductionPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(IntroductionPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
